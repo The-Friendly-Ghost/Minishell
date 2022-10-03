b386187d171d5de8dce69afe216314fbfcf57aea
Paul Niezen <pniezen@f0r5s15.codam.nl>
2022-10-03 11:24:29 +0200

    major changes

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: pniezen <pniezen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/24 14:44:45 by pniezen       #+#    #+#                 */
-/*   Updated: 2022/10/03 11:13:43 by pniezen       ########   odam.nl         */
+/*   Updated: 2022/10/03 11:23:12 by pniezen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,7 +81,9 @@ char			*id_env_var(char *str);
 char			*expand_env_var(char *name, char *value, char *str,
 					unsigned int i);
 
-//parser/parser.c
+// lexer/
+char			**lexer(char *input);
+char			**tokenizer(char *input, int token_count);
 
 //parser/
 t_token			**parser(char **input);
@@ -90,7 +92,7 @@ t_token			**parser(char **input);
 int				ft_strcmp(const char *s1, const char *s2);
 
 //parser/determine_token_type.c
-int	determine_token_type(char *token);
+int				determine_token_type(char *token);
 
 //parser/destroy_parser.c
 void			destroy_token_array(char **token_array);
@@ -98,9 +100,6 @@ void			destroy_token_list(t_token **token_list);
 
 // parser/parser_syntax_error.c
 bool			check_for_syntax_error(t_token **token_list);
-
-// expander/expander.c
-void			expander(t_token **token_list);
 
 //environment/get_env_data.c
 t_program		*get_program(void);
@@ -115,5 +114,8 @@ bool			check_if_env_has_value(const char *str);
 //builtin/
 void			print_env(void);
 void			print_pwd(void);
+
+//test_functions
+void			print_token_list(t_token **token_list);
 
 #endif
