91e6989b71e70e3fa178cd781cdc18658665344c
Paul Niezen <pniezen@f0r5s15.codam.nl>
2022-10-20 10:38:28 +0200

    removed weird function

--- src/executor/redirects.c
+++ src/executor/redirects.c
@@ -6,7 +6,7 @@
 /*   By: pniezen <pniezen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/18 13:20:25 by pniezen       #+#    #+#                 */
-/*   Updated: 2022/10/19 14:48:51 by pniezen       ########   odam.nl         */
+/*   Updated: 2022/10/19 14:53:16 by pniezen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,40 +37,4 @@ int	check_redirect(t_token *token_list)
 		temp = temp->next;
 	}
 	return (1);
-}
-
-char	**itterate_redirect(t_token *token_list, char *cmd)
-{
-	t_token	*temp;
-	char	*tmp;
-	char	*file;
-	char	**new_argv;
-
-	temp = token_list;
-	file = NULL;
-	if (temp->type == redirect_input)
-		return (NULL);
-	temp = temp->next;
-	while (temp)
-	{
-		if (file)
-			free(file);
-		if (temp->next && temp->type == redirect_input)
-		{
-			if (access(temp->next->content, F_OK | R_OK | W_OK) == -1)
-				return ((void)printf(
-						"minishell: %s: No such file or directory\n",
-						temp->next->content), set_exit_code(1), NULL);
-			temp = temp->next;
-			continue ;
-		}
-		tmp = ft_strdup(temp->content);
-		file = ft_strjoin(" ", tmp);
-		free(tmp);
-		temp = temp->next;
-	}
-	tmp = file;
-	file = ft_strjoin(ft_strdup(cmd), tmp);
-	new_argv = ft_split(file, ' ');
-	return (free(file), free(tmp), new_argv);
 }
